3b273dbe5d8a8845a074d788cbb3fbf1688b0fbb
yayanndhr <yayannadhirkalamm@gmail.com>
2024-11-09 11:25:25 +0800

    Add files via upload

---
+++ prog_tugas5.py
@@ -0,0 +1,15 @@
+#jenis-jenis Variabel, tipe data 
+x = 9 # integer, bilangan bulat
+print("tipe data dari x", type(x))
+
+y = 9.5 # float, bilangan 
+print("tipe data dari y", type(y))
+
+z = "pak edy" # String, kumpulan karakter
+print("tipe data dari z", type(z))
+
+a = True # Boolean, hanya 2, yes or no
+print("tipe data dari a", type(a))
+
+b = "9.99999"
+print("tipe data dari b", type(b))
