14b2791adac509a192a6a622556cc41c2dfba5a5
yayanndhr <yayannadhirkalamm@gmail.com>
2024-11-09 11:32:10 +0800

    Add files via upload

---
+++ prog_tugas4.py
@@ -0,0 +1,13 @@
+# star
+
+# Deklarasi variabel dan assignment value
+y = 24  # nilai awal variabel y
+x = 18  # nilai awal variabel x
+
+# Hitung nilai c sebagai hasil dari a + b
+LUAS = y + x
+
+# Tampilkan nilai c
+print("Hasil dari y + x adalah:", LUAS)
+
+# end
