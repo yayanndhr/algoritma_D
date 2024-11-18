afa5bc97e73ab0c82528425ff096a130a6b00a3c
yayanndhr <yayannadhirkalamm@gmail.com>
2024-11-18 09:23:45 +0800

    Add files via upload

---
+++ prog_tugas6.py
@@ -0,0 +1,19 @@
+# Maukan pengguna
+
+s = 24
+print (s)
+S = 18
+print (S)
+l = 432
+print (l)
+
+#casting tipe data
+s = int(input("masukkan nilai s :"))
+print (" nilai s adalah : ", s, type(s))
+
+S = int(input("masukkan nilai S:"))
+print (" nilai S adalah : ", S, type(S))
+
+l = int(input("masukkan nilai l:"))
+print (" nilai l adalah : ", l, type(l))
+
